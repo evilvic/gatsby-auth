a37d31638aca5a36eaa69540a6737f56adab0a78
Víctor Peña Romero <vic.pero@icloud.com>
2021-01-09 19:06:31 -0600

    refactor navigation component

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,13 +1,34 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, navigate } from 'gatsby'
+import { getUser, isLoggedIn, logout } from '../services/auth'
 
 const Navigation = () => {
+
+  let greetingMessage = ''
+  if (isLoggedIn()) {
+    greetingMessage = `Hello ${getUser().name}`
+  } else {
+    greetingMessage = 'You are not logged in'
+  }
+
   return (
     <>
-      <span>You are not logged in</span>
-      <Link to='/'>Home</Link>
-      <Link to='/'>Profile</Link>
-      <Link to='/'>Logout</Link>
+      <span>{greetingMessage}</span>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/app/profile'>Profile</Link>
+        {isLoggedIn() ? (
+          <a
+            href='/'
+            onClick={e => {
+              e.preventDefault()
+              logout(() => navigate('/app/login'))
+            }}
+          >
+            Logout
+          </a>
+        ) : null}
+      </nav>
     </>
   )
 }
