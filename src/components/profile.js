a3e233a624d1fee1b9cd0875d4ac8a5570360582
Víctor Peña Romero <vic.pero@icloud.com>
2021-01-09 18:49:16 -0600

    create router for app and login/profile components

---
+++ src/components/profile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <>
+      <h1>Your profile</h1>
+      <ul>
+        <li>Name: Your name will appear here</li>
+        <li>Email: And here goes the mail</li>
+      </ul>
+    </>
+  )
+}
+
+export default Profile
