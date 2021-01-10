f15fb1f0204639338a8727212ba673772912bc1d
Víctor Peña Romero <vic.pero@icloud.com>
2021-01-10 15:07:23 -0600

    add logout

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { isAuthenticated, getProfile } from '../utils/auth'
+import { isAuthenticated, getProfile, login, logout } from '../utils/auth'
 
 const Navigation = () => {
 
@@ -17,6 +17,11 @@ const Navigation = () => {
       <nav>
         <Link to='/'>Home</Link>
         <Link to='/app/profile'>Profile</Link>
+        {!isAuthenticated() ? (
+          <button onClick={() => login()}>Log in</button>
+        ) : (
+          <button onClick={() => logout()}>Log out</button>
+        )}
       </nav>
     </>
   )
