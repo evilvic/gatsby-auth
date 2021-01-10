5e7f8fe1f8ddce3e5d68f475a5953ec29012d24f
Víctor Peña Romero <vic.pero@icloud.com>
2021-01-09 18:57:59 -0600

    change app router

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Router } from '@reach/router'
 import Layout from '../components/layout'
+import Private from '../components/private'
 import Profile from '../components/profile'
 import Login from '../components/login'
 
@@ -8,7 +9,7 @@ const App = () => {
   return (
     <Layout>
       <Router>
-        <Profile path='/app/profile'/>
+        <Private path='/app/profile' component={Profile}/>
         <Login path='/app/login'/>
       </Router>
     </Layout>
