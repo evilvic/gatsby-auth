060fea8daf1981179de125bd337ff641abd5fee3
Víctor Peña Romero <vic.pero@icloud.com>
2021-01-09 21:27:08 -0600

    add auth0 config

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,18 @@ exports.onCreatePage = async ({ page, actions }) => {
     createPage(page)
   }
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /auth0-js/,
+            use: loaders.null()
+          }
+        ]
+      }
+    })
+  }
+}
